302613668ac9e36270261f5860d992a267df478e
MaryannGitonga <naygitonga4@gmail.com>
2018-09-17 14:15:13 +0300

    Add a timer pipe to detect time that has elapsed since the quote was created

---
+++ src/app/time-counter.pipe.ts
@@ -0,0 +1,29 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timeCounter'
+})
+export class TimeCounterPipe implements PipeTransform {
+
+  transform(value: any): any {
+    let days,hours,minutes;
+    if(value <= 59){
+      return `a few seconds ago`;
+    }
+    else if(value >= 60 && value <= 3599){
+      minutes = Math.round(value/60);
+      if(minutes == 1){
+        return `${minutes} minute ago`;
+      }else{
+        return `${minutes} minutes ago`;
+      }
+    }else{
+      hours = Math.round(value/3600);
+      if(hours == 1){
+        return `${hours} hour ago`;
+      }else{
+        return `${hours} hours ago`;
+      }
+    }
+  }
+}
